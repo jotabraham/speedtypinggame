7ce54451f6f38cee23c8f5c4168f547e33bab315
John Abe <jotabraham@gmail.com>
2020-10-23 16:39:37 -0400

    modified html and created js within new dir

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Global vars to use in functions
+let time = 5;
+let score = 0;
+let isPlaying;
+
+// DOM Elements
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
